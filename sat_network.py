4a7baf34d72672ce34f0658b7665e4367b12657b
Pranav Page <pranavpage33@gmail.com>
2022-05-07 12:53:03 +0530

    Added direction estimation (Phase 1)

--- sat_network.py
+++ sat_network.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 #need to build a satellite network with nodes and (lat, long)
 alt = 600e3 #600 km above the earth
-P = 10 #number of orbital planes
+P = 12 #number of orbital planes
 h = alt*np.ones(P) #altitude
-num_sats = 25 #satellites per plane 
+num_sats = 24 #satellites per plane 
 inclination = 90
 #set up a network of virtual nodes
 R = 6.378e6
@@ -36,7 +36,14 @@ class satellite:
         self.y = 0
         self.z = 0
         self.latitude = (np.pi/2 - self.polar_angle + 2*np.pi)%(2*np.pi)
-
+class packet:
+    def __init__(self, p1, s1, p2, s2, t):
+        self.p1 = p1 
+        self.s1 = s1 
+        self.p2 = p2 
+        self.s2 = s2
+        self.hops = 0
+        self.t_origin = t
 def initialize_constellation(alt, P, num_sats, inclination):
     planes = [] 
     sats = []
@@ -72,6 +79,74 @@ def plot_constellation(sats, dir_sats):
     return coordinates
 planes, sats, dir_sats = initialize_constellation(alt, P, num_sats, inclination)
 print(inclination)
-coords = plot_constellation(sats, dir_sats)
-plt.savefig("sat_constellation_vp.png")
-plt.show()
+#coords = plot_constellation(sats, dir_sats)
+#plt.savefig("sat_constellation_vp.png")
+#plt.show()
+
+# each vp has a buffer queue
+# pkt generated knows source and destination (p,s)
+# ph does not cross polar region 
+# pv does
+class min_path:
+    def __init__(self, dv, dh, nv, nh):
+        self.dv = dv 
+        self.dh = dh
+        self.nv = nv 
+        self.nh = nh 
+        self.hops = 0
+    def __repr__(self):
+        return f"dv={self.dv}, dh={self.dh}, nv={self.nv}, nh={self.nh}, hops={self.hops} \n"
+def direction_estimation(p1, s1, p2, s2):
+    ph = min_path(0,0,0,0)
+    pv = min_path(0,0,0,0)
+    if(s1 < num_sats/2 and s2 < num_sats/2):
+        #Eastern Hemisphere
+        ph.dh = (+1)*(p1 < p2) + (-1)*(p1 > p2)
+        ph.dv = (-1)*(s1 < s2) + (+1)*(s1 > s2)
+        ph.nv = abs(s1 - s2)
+        ph.nh = abs(p1 - p2)
+        ph.hops = ph.nv + ph.nh
+        
+        pv.dh = (-1)*(p1 < p2) + (+1)*(p1 >= p2)
+        pv.dv = (+1)*(2*(s1+s2+1) < num_sats) + (-1)*(2*(s1+s2+1) > num_sats) + (+1)*(2*(s1+s2+1) == num_sats)
+        pv.nh = (P - abs(p1-p2))
+        pv.nv = min((s1 + s2 + 1), num_sats - (s1 + s2 + 1))
+        pv.hops = pv.nv + pv.nh
+        pass
+    elif(s1 >= num_sats/2 and s2 >= num_sats/2):
+        #Western Hemisphere
+        ph.dh = (+1)*(p1 < p2) + (-1)*(p1 > p2)
+        ph.dv = (-1)*(s1 < s2) + (+1)*(s1 > s2)
+        ph.nv = abs(s1 - s2)
+        ph.nh = abs(p1 - p2)
+        ph.hops = ph.nv + ph.nh
+        
+        pv.dh = (-1)*(p1 < p2) + (+1)*(p1 >= p2)
+        pv.dv = (+1)*(2*(s1+s2+1) < 3*num_sats) + (-1)*(2*(s1+s2+1) >= 3*num_sats)
+        pv.nh = (P - abs(p1-p2))
+        pv.nv = min((s1 + s2 + 1) - num_sats,2*num_sats - (s1 + s2 + 1))
+        pv.hops = pv.nv + pv.nh
+        pass
+    else:
+        #Different sides of the seam
+        ph.dh = (-1)*(p1 < p2) + (+1)*(p1 > p2)
+        ph.dv = (+1)*((num_sats - 1 - s1) < s2) + (-1)*((num_sats - 1 - s1) > s2)
+        ph.nv = abs(num_sats-s1 - s2-1)
+        ph.nh = abs(p1 - p2) + num_sats
+        ph.hops = ph.nv + ph.nh
+        
+        if(s1 < num_sats/2 and s2 >= num_sats/2):
+            #1 East, 2 West
+            pv.dh = (+1)*(p1 < p2) + (-1)*(p1 > p2)
+            pv.dv = (-1)*(2*(s1-s2) < num_sats) + (+1)*(2*(s1-s2) >= num_sats)
+        elif(s1 >= num_sats/2 and s2 < num_sats/2):
+            #1 West, 2 East
+            pv.dh = (+1)*(p1 < p2) + (-1)*(p1 > p2)
+            pv.dv = (+1)*(2*(s1-s2) < num_sats) + (-1)*(2*(s1-s2) >= num_sats)
+        
+        pv.nh = abs(p1 - p2)
+        pv.nv = min(abs(s1-s2), num_sats - abs(s1-s2))
+        pv.hops = pv.nv + pv.nh        
+    return ph, pv
+ph, pv = direction_estimation(2, 8, 5, 6)
+print(ph, pv)
