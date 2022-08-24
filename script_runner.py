95aed0931368b94de03becc5a24c30bb035061b4
Pranav Page <pranavpage33@gmail.com>
2022-08-24 23:28:28 +0530

    Misc params changed

--- script_runner.py
+++ script_runner.py
@@ -1,8 +1,8 @@
 # Test script to run .py files on Windows
 import subprocess
 import os
-# os.remove('sim_log.csv', shell=True)
-for i in range(1):
+# os.remove('sim_log.csv')
+for i in range(20):
   subprocess.call("python3 main_script_simulation.py 0", shell=True)
   subprocess.call("python3 main_script_simulation.py 2", shell=True)
 subprocess.call("python3 sim_analysis.py", shell=True)
